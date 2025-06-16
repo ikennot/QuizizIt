17df03c06362c3e3ca6e1b7b56cdf2ed34f18b43
ikennot <domdomkenneth23@gmail.com>
2025-06-16 15:20:10 +0800

    testing

--- publicFolder/js-folder/data.js
+++ publicFolder/js-folder/data.js
@@ -9,18 +9,19 @@ let objectQuestion = {
     ]
 }
 let html ='';
-// function showOptions(){
+function showOptions(){
    
-//     objectQuestion.options.forEach((option)=>{
-//         html+=`${option}`
-//     })
-// }
-
-// showOptions();
+    objectQuestion.options.forEach((option)=>{
+        html+=` <div class="flex justify-center items-center bg-blue-300">${option}</div>`
+    })
+}
+  
+showOptions();
 document.querySelector('#question-container').innerHTML = `
   <p>${objectQuestion.question}</p>
+  <div class="grid grid-cols-4 m-4 gap-4">
    ${html}
-   
+   </div>
 
 
 `;
