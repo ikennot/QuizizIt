ba39aeae7a95d28e30971e11768f5902c1dd408b
ikennot <domdomkenneth23@gmail.com>
2025-06-25 09:25:27 +0800

    debugging

--- publicFolder/js-folder/MathQuestions.js
+++ publicFolder/js-folder/MathQuestions.js
@@ -2,6 +2,9 @@
 import { questionList } from "./Quiz-data.js";
 let html =  '';
 let i = 0;
+let length = questionList.length-1;
+let wrong = document.querySelector('.wrong');
+let correct = document.querySelector('.correct');
 function questionnare(i){
    let questionNum = document.querySelector('.questionNum-js');
    let question = document.querySelector('.question-js');
@@ -19,7 +22,23 @@ buttonOption.innerHTML = html;
   
 
 document.querySelectorAll('.ansButton-js').forEach((button)=>{
-  
+   
+   button.addEventListener('click',()=>{
+      let {answer} = button.dataset;
+      
+      if(answer === questionList[i].ans && i < length){
+         correct.play();
+        html = '';
+        questionnare(++i);
+      }else if(answer ==! questionList[i].ans && i < length)
+        {
+            wrong.play();
+            html = '';
+            questionnare(i);
+        }else{
+            window.location.href = `./MainMenu.html`
+        }
+   })
 })
 
 }
