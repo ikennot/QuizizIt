1e4b6f4709c957b9579548bad6ba2fc13dfc9b1f
ikennot <domdomkenneth23@gmail.com>
2025-06-25 21:44:11 +0800

    creating exit button for score

--- publicFolder/js-folder/MathQuestions.js
+++ publicFolder/js-folder/MathQuestions.js
@@ -5,6 +5,8 @@ let i = 0;
 let length = questionList.length-1;
 let wrong = document.querySelector('.wrong');
 let correct = document.querySelector('.correct');
+let score = 0;
+
 function questionnare(i){
    let questionNum = document.querySelector('.questionNum-js');
    let question = document.querySelector('.question-js');
@@ -25,8 +27,8 @@ document.querySelectorAll('.ansButton-js').forEach((button)=>{
    
    button.addEventListener('click',()=>{
       let {answer} = button.dataset;
-      (answer === questionList[i].ans)? correct.play() : wrong.play();
-  
+      (answer === questionList[i].ans)? (correct.play(), score++) : wrong.play();
+      
 
         if(i < length) {    
         setTimeout(()=>{
@@ -35,14 +37,27 @@ document.querySelectorAll('.ansButton-js').forEach((button)=>{
           },1000);
          }else{
             setTimeout(()=>{
-           window.location.href = `./MainMenu`
+         //   window.location.href = `./MainMenu`
+         DisplayScore();
           },2000);
            
          }
 
    })
 })
+}
 
+function DisplayScore(){
+   let mainContainer = document.querySelector('.mainBody');
+   mainContainer.innerHTML = `<div class="bg-blue-950 w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] rounded-2xl flex flex-col items-center justify-center p-5 gap-5">
+    <p class=" text-[20px] sm:text-4xl font-bold">Quiz Completed</p>
+    <img src="./assets/icons/check.png" class=" h-[50px] w-[50px] sm:h-[150px] sm:w-[150px]">
+    <p class="text-[15px] sm:text-3xl">Your Score</p>
+    <p class="text-[30px] sm:text-6xl font-bold">${score}/${length+1}</p>
+    <button class="button exitButton-js">Exit</button>
+  </div>`;
+
+  let button = document.querySelector('.exitButton-js');
 }
 
 questionnare(i);
