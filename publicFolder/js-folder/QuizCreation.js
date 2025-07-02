3d8efde313bb95dc48f668b00038cba2bddc9edf
ikennot <domdomkenneth23@gmail.com>
2025-07-02 16:15:26 +0800

    button functionality

--- publicFolder/js-folder/QuizCreation.js
+++ publicFolder/js-folder/QuizCreation.js
@@ -1,13 +1,15 @@
 import { saveLocalStorage,subjectList } from "./Quiz-data.js";
+let questionIndex = 0;  
+
 
 let subject = {
     category: ``,
     items: 0,
     questionList : [] 
 }
-
+function CreateQuestion(){
 document.querySelector('.QuestionContainer-js').innerHTML = `
-<p class="text-2xl font-bold">Question 1</p>
+<p class="text-2xl font-bold">Question ${++questionIndex}</p>
  <input type="text" placeholder="Enter Question" class="bg-white text-black p-1.5 sm:p-2.5 sm:w-[400px]">
   <div class=" flex flex-col items-center gap-1.5">
     <input type="text" placeholder="Option 1" class="bg-white text-black p-1.5 sm:p-2.5 option1">
@@ -16,7 +18,19 @@ document.querySelector('.QuestionContainer-js').innerHTML = `
     <input type="text" placeholder="Option 4" class="bg-white text-black p-1.5 sm:p-2.5 option4">
   </div>
   <div class="flex flex-row items-center gap-3">
-    <button class="button">Done</button>
-    <button class="button">next Question</button>
+    <button class="button button-done">Done</button>
+    <button class="button button-next">Next Question</button>
   </div>
 `
+ const nextButton = document.querySelector('.button-next');
+ nextButton.addEventListener('click',()=>{
+  CreateQuestion();
+ })
+
+ const doneButton = document.querySelector('.button-done');
+ doneButton.addEventListener('click',()=>{
+  
+ })
+}
+
+CreateQuestion();
