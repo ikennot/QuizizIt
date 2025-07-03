74d062aeeba87d6e9de8a3a704afe59ad0a0eee5
ikennot <domdomkenneth23@gmail.com>
2025-07-03 14:30:28 +0800

    creating button for answer validation

--- publicFolder/js-folder/QuizCreation.js
+++ publicFolder/js-folder/QuizCreation.js
@@ -12,16 +12,18 @@ document.querySelector('.QuestionContainer-js').innerHTML = `
 <p class="text-2xl font-bold">Question ${++questionIndex}</p>
  <input type="text" placeholder="Enter Question" class="bg-white text-black p-1.5 sm:p-2.5 sm:w-[400px]">
   <div class=" flex flex-col items-center gap-1.5">
-    <input type="text" placeholder="Option 1" class="bg-white text-black p-1.5 sm:p-2.5 option1">
-    <input type="text" placeholder="Option 2" class="bg-white text-black p-1.5 sm:p-2.5 option2">
-    <input type="text" placeholder="Option 3" class="bg-white text-black p-1.5 sm:p-2.5 option3">
-    <input type="text" placeholder="Option 4" class="bg-white text-black p-1.5 sm:p-2.5 option4">
+  <div class="flex flex-row items-center gap-2.5"><input type="text" placeholder="Option 1" class="bg-white text-black p-1.5 sm:p-2.5 option1"> <button class="chooseAnswerButton"></button></div> 
+  <div class="flex flex-row items-center gap-2.5"><input type="text" placeholder="Option 2" class="bg-white text-black p-1.5 sm:p-2.5 option1"> <button class="chooseAnswerButton"></button></div> 
+  <div class="flex flex-row items-center gap-2.5"><input type="text" placeholder="Option 3" class="bg-white text-black p-1.5 sm:p-2.5 option1"> <button class="chooseAnswerButton"></button></div> 
+  <div class="flex flex-row items-center gap-2.5"><input type="text" placeholder="Option 4" class="bg-white text-black p-1.5 sm:p-2.5 option1"> <button class="chooseAnswerButton"></button></div> 
   </div>
   <div class="flex flex-row items-center gap-3">
     <button class="button button-done">Done</button>
     <button class="button button-next">Next Question</button>
   </div>
 `
+ 
+
  const nextButton = document.querySelector('.button-next');
  nextButton.addEventListener('click',()=>{
 
