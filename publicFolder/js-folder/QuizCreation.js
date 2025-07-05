9e2604ae93b9ebb2c65696cbea9fcde2116d8446
ikennot <domdomkenneth23@gmail.com>
2025-07-05 15:46:52 +0800

    create an array subject.questionList

--- publicFolder/js-folder/QuizCreation.js
+++ publicFolder/js-folder/QuizCreation.js
@@ -8,6 +8,7 @@ let subject = {
     questionList : [] 
 }
 function CreateQuestion(){
+  let ans = '';
 document.querySelector('.QuestionContainer-js').innerHTML = `
 <p class="text-2xl font-bold">Question ${++questionIndex}</p>
  <input type="text" placeholder="Enter Question" class="bg-white text-black p-1.5 sm:p-2.5 sm:w-[400px] getQuestion">
@@ -26,26 +27,38 @@ document.querySelector('.QuestionContainer-js').innerHTML = `
   button.addEventListener('click',()=>{
     const {choose} = button.dataset;
     button.classList.add('bg-red-500'); 
-     
+    let num = 1;
     document.querySelectorAll('.chooseAnswerButton').forEach((removebutton)=>{
       const remove = removebutton.dataset.choose;
-
+    
       if(choose !== remove){
         removebutton.classList.remove('bg-red-500'); 
+      }else{
+        ans = document.querySelector(`.option${num}`).value;
       }
+
+      num++;
     })
-     let question = document.querySelector('.getQuestion').value;
-    subject.questionList.push(
-       question,
-        
-    )
+
   })
  })
 
  const nextButton = document.querySelector('.button-next');
  nextButton.addEventListener('click',()=>{
 
-  
+       const question = document.querySelector('.getQuestion').value;
+     const O1 = document.querySelector('.option1').value;
+     const O2 = document.querySelector('.option2').value;
+     const O3 = document.querySelector('.option3').value;
+     const O4 = document.querySelector('.option4').value;
+    subject.questionList.push(
+       {
+       question,
+       ans,
+       options : [O1,O2,O3,O4]
+       }
+    )
+  console.log(subject.questionList)
   CreateQuestion();
  })
 
